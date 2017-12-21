8764dffbc21691176b1460c034f5d4e1bd4f3f2a
desmondnzuza <desmondnzuza@gmail.com>
2017-12-21 10:22:20 +0200

    adds program logic to allow for the placement of orders

---
+++ src/PizzaStore/Helper/StringHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace PizzaStore.Helper
+{
+    public static class StringHelper
+    {
+        public static bool IsValidAnswer(this string answer)
+        {
+            var supportedAnswers = new []{"Cheese", "Greek", "Pepperoni"};
+            return supportedAnswers.Contains(answer);
+        }
+    }
+}
--- src/PizzaStore/Program.cs
+++ src/PizzaStore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PizzaStore.Helper;
 
 namespace PizzaStore
 {
@@ -6,7 +7,49 @@ namespace PizzaStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var endProgram = false;
+            var factory    = new Factory.SimplePizzaFactory();
+            var store      = new Store.PizzaStore(factory);
+
+            do
+            {
+                var answer = GetOrder();
+
+                if (answer.IsValidAnswer())
+                {
+                    Console.WriteLine("placing order...");
+                    var order = store.OrderPizza(answer);
+                    Console.WriteLine(String.Empty);
+                    Console.WriteLine($"You just ordered : {order.GetDescription()}");
+                    Console.WriteLine(string.Empty);
+                }
+                else
+                {
+                    endProgram = true;
+                }
+
+            } while (!endProgram);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        private static string GetOrder()
+        {
+            Console.WriteLine("What do you want to order? 1)Cheese 2)Greek 3) Pepperoni");
+            var rawAnswer = Console.ReadLine();
+
+            switch (rawAnswer)
+            {
+                case "1":
+                    return "Cheese";
+                case "2":
+                    return "Greek";
+                case "3":
+                    return "Pepperoni";
+                default:
+                    return rawAnswer;
+            }
         }
     }
 }
